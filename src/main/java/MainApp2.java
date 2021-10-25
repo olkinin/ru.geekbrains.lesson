0747a6e1b79c0373581a63dc9209cdcfe1c47a3d
olkinin <olkinin@mail.ru>
2021-10-25 21:10:46 +0300

    Initial commit

---
+++ src/main/java/MainApp2.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class MainApp2 {public static void main(String[] args) {
+
+    newMetod();
+    chislo();
+    number();
+    text();
+    Year();
+}
+
+    public static void newMetod() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число а и b");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if ((a + b) >= 10 && (a + b) <= 20) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+
+    public static void chislo() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите целое число i");
+        int i = scan.nextInt();
+        if (i >= 0) {
+            System.out.println("Число i - положительное");
+        } else {
+            System.out.println("Число i - отрицательное");
+        }
+    }
+
+    public static void number() {
+        Scanner as = new Scanner(System.in);
+        System.out.println("Введите целое число j");
+        int j = as.nextInt();
+        boolean c = j<0;
+        System.out.println(c);
+    }
+
+
+    public static void text() {
+        Scanner g = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String str=g.nextLine();
+        System.out.println("Введите целое число");
+        int n = g.nextInt();
+        int i = 0;
+        while (i<n) {
+            System.out.println(str);i++;
+        }}
+    public static void Year() {
+        System.out.println("Введите год");
+        Scanner y = new Scanner(System.in);
+        int year = y.nextInt();
+        boolean c = year%400==0 && year%100==0 && year%4==0;
+        System.out.println(c);
+}}
